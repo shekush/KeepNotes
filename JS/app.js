bc3fe72fc06ee2a94860b755388700d35800019a
Shejal <shejal270999@gmail.com>
2021-11-23 21:31:30 +0530

    Added the feature that shows the notes from the local storage.

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,54 @@
-console.log("This is included without any error")
+console.log("This is included without any error");
+showNotes();
+//if the user adds any note, add it to the local storage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {  //e in this function is an event object
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+    showNotes();
+});
+
+//function to show elements from the local storage
+
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+                <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">Note ${index + 1}</h5>
+                <p class="card-text">${element}</p>
+                <button href="#" class="btn btn-primary">Delete Note</button>
+                </div>
+                </div>
+                `;
+    });
+    let notesElem = document.getElementById("notes");
+    if(notesObj.length != 0)
+    {
+        notesElem.innerHTML = html;
+    }
+    else{
+        notesElem.innerHTML = `Nothing to display.`;
+    }
+}
+
+//function to delete a note
+
