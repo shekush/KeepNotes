673bc78655283109dae9d305d8a20fe7dcb495d3
Shejal <shejal270999@gmail.com>
2021-11-23 21:44:37 +0530

    Added the feature of deleting notes

--- JS/app.js
+++ JS/app.js
@@ -35,20 +35,32 @@ function showNotes() {
                 <div class="card-body">
                 <h5 class="card-title">Note ${index + 1}</h5>
                 <p class="card-text">${element}</p>
-                <button href="#" class="btn btn-primary">Delete Note</button>
+                <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
                 </div>
                 `;
     });
     let notesElem = document.getElementById("notes");
-    if(notesObj.length != 0)
-    {
+    if (notesObj.length != 0) {
         notesElem.innerHTML = html;
     }
-    else{
+    else {
         notesElem.innerHTML = `Nothing to display.`;
     }
 }
 
 //function to delete a note
 
+function deleteNote(index) {
+    console.log("deleted a note", index);
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
